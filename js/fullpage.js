e747813c43b039e0e90041c87a4856f1536beb22
wousser <wousser@icloud.com>
2016-11-02 15:49:01 +0800

    New Website

---
+++ js/fullpage.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['AboutMe', 'Huawei', 'SonyEricsson', 'HappyFish', 'Prototyping', 'MyWork'],
+    sectionsColor: ['#FFF6E5', '#2185C6', '#FF7E65', '#2185C6', '#3D454C'],
+    navigation: false,
+    navigationPosition: 'right',
+    navigationTooltips: ['About Me', 'Huawei', 'Sony Ericsson', 'Happy Fish', 'Prototyping', 'My Work'],
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: false,
+    fitToSection: true,
+    fitToSectionDelay: 300,
+    showActiveTooltip: false
+  });
+});
--- portfolio.md
+++
@@ -1,5 +0,0 @@
----
-title: Portfolio
-date: 
-layout: ''
----
